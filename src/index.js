cb2ecc4bef978ccd91ec2cfa0be16f568726a6e5
luigy-mach <luigy.mach.arc@gmail.com>
2018-11-03 11:18:53 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import './index.css';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//// If you want your app to work offline and load faster, you can change
+//// unregister() to register() below. Note this comes with some pitfalls.
+//// Learn more about service workers: http://bit.ly/CRA-PWA
+//serviceWorker.unregister();
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import createHashHistory from 'history/createHashHistory';
+  
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
+    
+    ReactDOM.render(
+        <Router history={hashHistory}>
+          <App />
+        </Router>,
+      document.getElementById('root') as HTMLElement
+    );
