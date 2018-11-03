34731ab88b9b7136bc0885b777d275ae1fbc44f3
luigy-mach <luigy.mach.arc@gmail.com>
2018-11-03 08:39:33 -0500

    Updates

---
+++ precache-manifest.ae0ef4e6870de933bd459e939a536361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/luigy.machaca.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0524e6f2e99e37262f74",
+    "url": "/luigy.machaca.github.io/static/js/runtime~main.0524e6f2.js"
+  },
+  {
+    "revision": "257a4aad75181590e869",
+    "url": "/luigy.machaca.github.io/static/js/main.257a4aad.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/luigy.machaca.github.io/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "257a4aad75181590e869",
+    "url": "/luigy.machaca.github.io/static/css/main.a33771b6.chunk.css"
+  },
+  {
+    "revision": "7238b69f1b2b024bf3d7c4b9c64307b8",
+    "url": "/luigy.machaca.github.io/index.html"
+  }
+];
